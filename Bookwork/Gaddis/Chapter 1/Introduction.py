dce6dd7de5d0fc411b74ea7ad327553cad128f43
Ramiro Gonzalez <rgonzalez69@ucmerced.edu>
2018-12-05 13:58:32 -0800

    Add files via upload

---
+++ Bookwork/Gaddis/Chapter 1/Introduction.py
@@ -0,0 +1,66 @@
+
+# coding: utf-8
+
+# Ramiro Gonzalez <br> 
+
+# Chapter 1
+
+# Gaddis, T. (2019). Starting out with Python. New York, NY: Pearson
+
+# In[7]:
+
+
+
+print("Python programming is fun")
+
+
+# In[8]:
+
+
+print("To be or not to be")
+
+
+# In[9]:
+
+
+print("That is the question")
+
+
+# # 
+
+# In[1]:
+
+
+print('This is a test of the Python interpreter')
+
+
+# #
+
+# In[2]:
+
+
+print('This is a test of IDLE')
+
+
+# #
+
+# 11 is 1011 <br> 
+# 65 is 0100 0001 <br> 
+# 100 is 0110 0100 <br> 
+# 255 is 1111 1111 <br> 
+
+# # 
+
+# 1101 is 13 <br> 
+# 1000 is 8 <br> 
+# 0010 1011 is 43<br> 
+
+# # 
+
+# ACII: 82 65 77 73 82 79 
+
+# #
+
+# Guido van Rossum is the creator of Python.<br> 
+# Python was created December 1989 <br> 
+# BDFL is Benevolent Dictator For Life, it means the BDFL has decides what to add or reject to the Open Source project <br> 
